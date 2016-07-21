2769b77f5335500a9a79b55808ba6c1e8989eed0
Tim Tregubov <tim@zingweb.com>
2016-07-21 13:07:17 -0400

    adding in react router

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import Welcome from './welcome';
-
 // example class based component (smart component)
 class App extends Component {
   constructor(props) {
@@ -14,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Welcome />
+        {this.props.children}
       </div>
     );
   }
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Welcome = () => {
   return (
     <div>
-      react+webpack+babel+eslint starter
+      react+react-router+webpack+babel+eslint starter
     </div>
   );
 };
